298a518d50bc13f7f8344c792813b1d00a437e20
Gosqu248 <grzegorzurban248@gmail.com>
2024-01-18 18:26:37 +0100

    HomeScreen done

--- src/main/java/com/javappa/start/HomeController.java
+++ src/main/java/com/javappa/start/HomeController.java
@@ -4,6 +4,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+
 import java.util.Arrays;
 import java.util.List;
 
@@ -17,6 +18,23 @@ public class HomeController {
 
         List<String> navLinks = Arrays.asList("/", "/Plany Treningowe","/Kalkulator Kalorii","/Porady");
         model.addAttribute("navLinks", navLinks);
+
+        model.addAttribute("welcome", "Witamy na stronie GymHelper !");
+
+        model.addAttribute("sectionText", "Strona została stworzona dla amatorów, którzy chcą zacząć z siłownią, a nie wiedzą jak się za to wziąć, a także dla tych, którzy chcą sobie przypomnieć wiedzę lub dowiedzieć się czegoś nowego.");
+
+        List<String> pageFeatures = Arrays.asList(
+                "🔸 Gotowe plany treningowe, dla różnego stopnia zaawansowania",
+                "🔸 Kalkulator kalorii, dzięki czemu możesz obliczyć swoje spożyte kalorie",
+                "🔸 Porady: jak zacząć z siłownią, jak zacząć redukować, jak zacząć masować");
+        model.addAttribute("pageFeatures", pageFeatures);
+
+        model.addAttribute("what", "Na stronie znajdziesz przede wszystkim:");
+
+        model.addAttribute("footerText", "CORPORATE FITNESS 🔸 NUTRITIONAL ADVICE 🔸 WEIGHT LOSS 🔸 MUSCLE TONE 🔸 CORE STRENGTH 🔸 POSTURE CORRECTION 🔸 CARDIO FITNESS");
+
+        model.addAttribute("endText", "@ 2035 by GymHelper");
+
         return "hello";
     }
 }
