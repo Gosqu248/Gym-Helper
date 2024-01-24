834901a54d21219afa3a2856284e81736e99cfe5
Qnewek <dawidkusion2002@gmail.com>
2024-01-24 19:47:55 +0100

    Progress so far

---
+++ src/main/java/com/javappa/start/Day.java
@@ -0,0 +1,20 @@
+package com.javappa.start;
+
+public class Day {
+    private String label;
+    private int value;
+
+    public Day(String label, int value) {
+        this.label = label;
+        this.value = value;
+    }
+
+    public String getLabel() {
+        return label;
+    }
+
+    public int getValue() {
+        return value;
+    }
+
+}
--- src/main/java/com/javappa/start/KalkulatorKaloriiController.java
+++ src/main/java/com/javappa/start/KalkulatorKaloriiController.java
@@ -2,15 +2,29 @@ package com.javappa.start;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Map;
 
 @Controller
 public class KalkulatorKaloriiController {
+    private List<Day> circleContentsPrev = List.of(
+            new Day("N", 1),
+            new Day("B", 2),
+            new Day("M", 3),
+            new Day("D", 4),
+            new Day("B", 5),
+            new Day("F", 6),
+            new Day("G", 7),
+            new Day("H", 8),
+            new Day("b", 9),
+            new Day("J", 10)
+    );
+
     @GetMapping("/Kalkulator Kalorii")
     public String calculator(Model model) {
         model.addAttribute("logo", "Gym Helper");
@@ -23,7 +37,19 @@ public class KalkulatorKaloriiController {
         navLinks.add("/Porady");
         model.addAttribute("navLinks", navLinks);
 
+        model.addAttribute("circleContents", circleContentsPrev);
+
+        model.addAttribute("footerText", "CORPORATE FITNESS 🔸 NUTRITIONAL ADVICE 🔸 WEIGHT LOSS 🔸 MUSCLE TONE 🔸 CORE STRENGTH 🔸 POSTURE CORRECTION 🔸 CARDIO FITNESS");
+
+        model.addAttribute("endText", "@ 2035 by GymHelper");
+
         return "calorie_calculator";
     }
-
+    @PostMapping("/updateCircle")
+    @ResponseBody
+    public String updateCircle(@RequestBody Map<String, String> circleData, Model model) {
+        String currentCircle = circleData.get("label") + "\n" + circleData.get("value");
+        return currentCircle;
+    }
 }
+
